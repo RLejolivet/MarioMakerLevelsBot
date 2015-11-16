34932294cab8af6d011a39c371f49a3884991886
Raphaël LEJOLIVET <raphael.lejolivet@gmail.com>
2015-11-16 23:22:07 +0100

    Added a log file because sometimes the compiled version wasn't working

--- MarioMakerLevelsBot/MarioMakerLevelsBot/MarioMakerLevelsBot.py
+++ MarioMakerLevelsBot/MarioMakerLevelsBot/MarioMakerLevelsBot.py
@@ -6,10 +6,16 @@ def main():
     from PySide import QtGui
     from LevelsBotWindow import LevelsBotWindow
 
+    logfile = open("log.txt", "w")
+    sys.stdout = logfile
+    sys.stderr = logfile
+
     app = QtGui.QApplication(sys.argv)
     win = LevelsBotWindow()
     win.show()
     app.exec_()
+
+    logfile.close()
 
 if(__name__ == "__main__"):
 
